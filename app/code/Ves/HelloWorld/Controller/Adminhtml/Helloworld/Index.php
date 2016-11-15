c9672012c7a1614e6edebb6f6e60ff12eb8a7629
alagumuthu <alagumuthu@capitalnumbers.com>
2016-11-15 22:14:55 +0530

    grid and product description page executed

--- app/code/Ves/HelloWorld/Controller/Adminhtml/Helloworld/Index.php
+++ app/code/Ves/HelloWorld/Controller/Adminhtml/Helloworld/Index.php
@@ -1,19 +1,29 @@
 <?php
  
-namespace Ves\HelloWorld\Controller\Adminhtml\News;
- 
-use Ves\HelloWorld\Controller\Adminhtml\News;
- 
-class Index extends \Ves\HelloWorld\Controller\Adminhtml\Helloworld
-{
-    /**
+namespace Ves\HelloWorld\Controller\Adminhtml\Helloworld;
+
+class Index extends \Magento\Backend\App\Action  /**
      * @return void
      */
+{
+const ADMIN_RESOURCE = 'Ves_HelloWorld::Requestquote';
+
+protected $resultPageFactory;
+
+public function __construct(
+            \Magento\Backend\App\Action\Context $context,
+            \Magento\Framework\View\Result\PageFactory $resultPageFactory
+){
+     parent::__construct($context);
+    $this->resultPageFactory = $resultPageFactory;
+}
+
    public function execute()
    {
-     $resultPage = $this->_resultPageFactory->create();
-
-        return $resultPage;
-  }
+        
+    $resultPage = $this->resultPageFactory->create();
+    $resultPage->getConfig()->getTitle()->prepend(__('Requestquote'));
+    return $resultPage;
+   }
 }
  
